00d7e8301348d94571b32927959fb3901942f8e0
João Paulo <felicio.joaopaulodev@gmail.com>
2025-07-28 10:48:59 -0300

    Removido projeto Estacionamento

--- Estacionamento/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Estacionamento.Models;
-
-RegistroEstacionamento registroEstacionamento = new RegistroEstacionamento();
-
-registroEstacionamento.CadastrarEntradaNoEstacionamento();
-registroEstacionamento.CalcularTotalEstacionamento();
-registroEstacionamento.ImprimirNotaFiscal();
